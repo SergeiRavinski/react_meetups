03d0c144bdf90b1ca8218a935d11a1a6a9bc082f
sergei <ravinski.sergei@gmail.com>
2023-08-14 13:15:09 +0200

    Some Changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Route } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
+
 function App() {
-	return <div></div>;
+	return (
+		<div>
+			<Route path="/">
+        <AllMeetupsPage />
+      </Route>
+      <Route path='/favorites'>
+        <FavoritesPage />
+      </Route>
+      <Route path='/new-meetup'>
+        <NewMeetupPage />
+      </Route>
+		</div>
+	);
 }
 
 export default App;
