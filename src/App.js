7d8f0055fe40f8fe9b2a2e7c4995b726f1500b4c
sergei <ravinski.sergei@gmail.com>
2023-08-14 14:28:22 +0200

    Added styles and data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
 	return (
 		<div>
-			<Route path="/">
-        <AllMeetupsPage />
-      </Route>
-      <Route path='/favorites'>
-        <FavoritesPage />
-      </Route>
-      <Route path='/new-meetup'>
-        <NewMeetupPage />
-      </Route>
+      <MainNavigation />
+			<Switch>
+				<Route path="/" exact>
+					<AllMeetupsPage />
+				</Route>
+				<Route path="/favorites">
+					<FavoritesPage />
+				</Route>
+				<Route path="/new-meetup">
+					<NewMeetupPage />
+				</Route>
+			</Switch>
 		</div>
 	);
 }
