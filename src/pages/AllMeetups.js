53a93496c99758405d65e40946d5be270919300f
sergei <ravinski.sergei@gmail.com>
2023-08-16 14:14:20 +0200

    Created function useEffect with fetcing data

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -3,6 +3,21 @@ import { useState, useEffect } from 'react';
 import MeetupList from '../components/meetups/MeetupList';
 
 function AllMeetupsPage() {
+
+	useEffect(() => {
+		setIsLoading(true);
+		async function fetchData() {
+			const endpoint =
+				'https://react-meetup-app-d5dbc-default-rtdb.europe-west1.firebasedatabase.app/meetups.json';
+			const response = await fetch(endpoint);
+			const data = await response.json();
+			setIsLoading(false);
+			setLoadedMeetups(data);
+		}
+		const result = fetchData();
+		console.log(result);
+	}, []);
+
 	if (isLoading) {
 		return (
 			<section>
