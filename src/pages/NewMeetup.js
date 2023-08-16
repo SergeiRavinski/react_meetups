96268b2def22950a0c0db5905a98223947be8a82
sergei <ravinski.sergei@gmail.com>
2023-08-16 12:49:00 +0200

    Created async function

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -3,8 +3,10 @@ import { useHistory } from 'react-router-dom';
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
-	function addMeetupHandler(meetupData) {
-		fetch(
+	const history = useHistory();
+
+	async function addMeetupHandler(meetupData) {
+		await fetch(
 			'https://react-meetup-app-d5dbc-default-rtdb.europe-west1.firebasedatabase.app/meetups.json',
 			{
 				method: 'POST',
