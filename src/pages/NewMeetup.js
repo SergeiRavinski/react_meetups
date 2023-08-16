ce59f0e176b6634a8c5ac2348fc761832a85da22
sergei <ravinski.sergei@gmail.com>
2023-08-16 11:56:41 +0200

    Created addMeetupHandler to sent POST request

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,22 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
+	function addMeetupHandler(meetupData) {
+		fetch(
+			'https://react-meetup-app-d5dbc-default-rtdb.europe-west1.firebasedatabase.app/meetups.json',
+			{
+				method: 'POST',
+				body: JSON.stringify(meetupData),
+				headers: {
+					'Content-Type': 'application/json',
+				},
+			}
+		);
+	}
 	return (
 		<section>
 			<h1>New Meetup Page</h1>
-			<NewMeetupForm />
+			<NewMeetupForm onAddNewMeetup={addMeetupHandler} />
 		</section>
 	);
 }
