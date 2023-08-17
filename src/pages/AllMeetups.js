34a4e0ef6f7b3a43283f053697d2c3824381070e
sergei <ravinski.sergei@gmail.com>
2023-08-17 11:07:50 +0200

    Rewrote useEffect function using then

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -8,16 +8,26 @@ function AllMeetupsPage() {
 
 	useEffect(() => {
 		setIsLoading(true);
-		async function fetchData() {
-			const endpoint =
-				'https://react-meetup-app-d5dbc-default-rtdb.europe-west1.firebasedatabase.app/meetups.json';
-			const response = await fetch(endpoint);
-			const data = await response.json();
-			setIsLoading(false);
-			setLoadedMeetups(data);
-		}
-		const result = fetchData();
-		console.log(result);
+		fetch(
+			'https://react-meetup-app-d5dbc-default-rtdb.europe-west1.firebasedatabase.app/meetups.json'
+		)
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				const meetups = [];
+
+				for (const key in data) {
+					const meetup = {
+						id: key,
+						...data[key],
+					};
+					meetups.push(meetup);
+				}
+
+				setIsLoading(false);
+				setLoadedMeetups(meetups);
+			});
 	}, []);
 
 	if (isLoading) {
