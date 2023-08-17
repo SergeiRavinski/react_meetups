cb7d7bd02e1c08ef39235d3712d298965cb5822e
sergei <ravinski.sergei@gmail.com>
2023-08-17 12:01:22 +0200

    Created the favoritesContextProvider function

--- src/store/favorites_content.js
+++ src/store/favorites_content.js
@@ -4,3 +4,19 @@ const FavoritesContext = createContext({
 	favorites: [],
 	totalFavorites: 0,
 });
+
+function favoritesContextProvider(props) {
+	const [userFavorites, setUserFavorites] = useState();
+	const context = {
+		favorites: userFavorites,
+		totalFavorites: userFavorites.length,
+	};
+
+	return (
+		<FavoritesContext.Provider value={context}>
+			{props.children}
+		</FavoritesContext.Provider>
+	);
+}
+
+export default favoritesContextProvider;
