f1b40aaf2c45231ac3993c6f3d78dc030261eb4c
sergei <ravinski.sergei@gmail.com>
2023-08-18 14:46:45 +0200

    Imported FavoritesContext and MeetupList

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,4 +1,8 @@
 import { useContext } from 'react';
+
+import FavoritesContext from '../store/favorites_content';
+import MeetupList from '../components/meetups/MeetupList';
+
 function FavoritesPage() {
   return <div>Favorites Page</div>;
 }
