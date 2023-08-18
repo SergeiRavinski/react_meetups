8235c7fe5092963bd5b533277b3741133079e9e3
sergei <ravinski.sergei@gmail.com>
2023-08-18 14:45:23 +0200

    Formated code

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
+
 import classes from './MainNavigation.module.css';
 import FavoritesContext from '../../store/favorites_content';
 
