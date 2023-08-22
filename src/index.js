d5124ed589183f5792b9beb304a7baeaa72982db
sergei <ravinski.sergei@gmail.com>
2023-08-22 13:23:37 +0200

    Imported reset.css

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import './variables.css';
+import './reset.css';
 import './index.css';
 import App from './App';
 import { FavoritesContextProvider } from './store/favorites_content';
